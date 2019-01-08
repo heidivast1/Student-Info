660712be9c1cdcba3b0166eb20b417ea060ecfba
heidivast1 <43884561+heidivast1@users.noreply.github.com>
2019-01-08 10:09:18 -0500

    refined searching

--- Desktop/student_info/students.js
+++ Desktop/student_info/students.js
@@ -33,22 +33,48 @@ var students = [
     achievements: 5,
     grades: 76
   },
-  
+
 ];
 
 var message = '';
-var student;  
+var student;
+var search;
+var notFound;
 
 function print(message) {
   var outputDiv = document.getElementById('output');
   outputDiv.innerHTML = message;
   }
-  
-for (var i = 0; i < students.length; i += 1) {
-  student = students[i];
-  message += '<h2>Student: ' + student['name'] + '</h2>';
-  message += '<p>Track: ' + student.track + '</p>';
-  message += '<p>Achievements: ' + student.achievements + '</p>';
-  message += '<p>Points: ' + student.grades + '</p>';
+
+function getStudentReport( student ) {
+  var report = '<h2>Student: ' + student['name'] + '</h2>';
+  report += '<p>Track: ' + student.track + '</p>';
+  report += '<p>Achievements: ' + student.achievements + '</p>';
+  report += '<p>Points: ' + student.grades + '</p>';
+  return report;
+  //*reminder of scope: var only lives in this function because defined there
+}
+
+while (true) {
+  search = prompt('Search student records: type a name [Jody] (or type quit to end)')
+  if (search === null || search.toLowerCase() === 'quit') {
+    break;
+  }
+  for (var i = 0; i < students.length; i += 1) {
+    student = students[i];
+    if (student.name === search ) {
+      message = getStudentReport(student);
+      notFound = false;
+      print(message);
+      break
+    } else {
+      notFound = true;
+    }
+  }
+  if (notFound === true && message === "") {
+    message = "Sorry, No student found with that name."
+    alert(message);
+    search = "quit";
+  }
 }
 print(message);
